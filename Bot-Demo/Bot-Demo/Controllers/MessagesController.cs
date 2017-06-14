f9d2ea90ba9adfe83f0c0d39e7bb693382578d6c
Jason Sowers <v-jassow@microsoft.com>
2017-06-14 10:45:49 -0700

    Moved code into controller from dialog

---
+++ Bot-Demo/Bot-Demo/Controllers/MessagesController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using AdaptiveCards;
+using Bot_Demo.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace Bot_Demo
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<Activity> HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                IConversationUpdateActivity iConversationUpdated = message as IConversationUpdateActivity;
+                if (iConversationUpdated != null)
+                {
+                    ConnectorClient connector = new ConnectorClient(new System.Uri(message.ServiceUrl));
+
+                    foreach (var member in iConversationUpdated.MembersAdded ??
+                                           System.Array.Empty<ChannelAccount>())
+                    {
+                        // if the bot is added, then 
+                        if (member.Id == message.Recipient.Id)
+                        {
+                            AdaptiveCard card =
+                                JsonConvert.DeserializeObject<AdaptiveCard>(
+                                    File.ReadAllText(HttpContext.Current.Server.MapPath("../Dialogs/menu.json")));
+                            Activity reply = null;
+
+                            // Create the attachment.
+                            Attachment attachment = new Attachment()
+                            {
+                                ContentType = AdaptiveCard.ContentType,
+                                Content = card
+                            };
+                            reply = ((Activity) iConversationUpdated).CreateReply();
+                            reply.Attachments = new List<Attachment>();
+                            reply.Attachments.Add(attachment);
+                            await connector.Conversations.ReplyToActivityAsync(reply);
+                        }
+                    }
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
